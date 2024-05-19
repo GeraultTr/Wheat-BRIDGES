e002ac0c913f362750bc748c273e1887cdaba136
Tristan Gérault <tristan.gerault@inrae.fr>
2024-05-19 14:52:17 +0200

    sub

--- test/test_cnwheat.py
+++ test/test_cnwheat.py
@@ -3,7 +3,7 @@ import os, sys
 import multiprocessing as mp
 # Model packages
 from wheat_bridges.wheat_bridges import Model
-# Utility packages
+    # Utility packages
 from initialize.initialize import MakeScenarios as ms
 from log.logging import Logger
 from analyze.analyze import analyze_data
@@ -15,7 +15,7 @@ def single_run(scenario, outputs_dirpath="outputs"):
     logger = Logger(model_instance=whole_plant, outputs_dirpath=outputs_dirpath, 
                     time_step_in_hours=1,
                     logging_period_in_hours=6,
-                    recording_images=False, plotted_property="C_hexose_root", show_soil=True,
+                    recording_images=True, recording_off_screen=True, plotted_property="C_hexose_root", show_soil=True,
                     recording_mtg=False,
                     recording_raw=False,
                     recording_sums=True,
@@ -24,7 +24,7 @@ def single_run(scenario, outputs_dirpath="outputs"):
                     echo=True)
 
     try:
-        for _ in range(200):
+        for _ in range(2500):
             # Placed here also to capture mtg initialization
             logger()
             logger.run_and_monitor_model_step()
@@ -45,7 +45,7 @@ def single_run(scenario, outputs_dirpath="outputs"):
 
 
 def test_apply_scenarios():
-    scenarios = ms.from_table(file_path="inputs/Scenarios.xlsx", which=["WB2", "WB3"])
+    scenarios = ms.from_table(file_path="inputs/Scenarios.xlsx", which=["WB1", "WB2", "WB3"])
     processes = []
     for scenario_name, scenario in scenarios.items():
         print(f"[INFO] Launching scenario {scenario_name}...")
