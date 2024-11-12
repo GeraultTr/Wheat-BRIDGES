a155eb58dc7347a516cd130ca4b379bc91fa2d5f
GeraultTr <tristan.gerault@inrae.fr>
2024-11-12 11:43:22 +0100

    pull_all

--- pull_all.sh
+++ pull_all.sh
@@ -1,3 +1,4 @@
+# Related to Root-BRIDGES
 cd Root_BRIDGES
 cd root_cynaps
 git pull origin main
@@ -8,6 +9,35 @@ cd ..
 git pull origin main
 cd ..
 git pull origin main
+
+# Related to WheatFspm
+cd WheatFspm
+cd adel
+git pull origin master
+cd ..
+cd cn-wheat
+git pull origin main
+cd ..
+cd elong-wheat
+git pull origin master
+cd ..
+cd farquhar-wheat
+git pull origin main
+cd ..
+cd fspm-wheat
+git pull origin main
+cd ..
+cd growth-wheat
+git pull origin master
+cd ..
+cd respi-wheat
+git pull origin master
+cd ..
+cd senesc-wheat
+git pull origin master
+cd ..
+git pull origin master
+cd ..
 
 # Optionnal, only if data_utility is in the same folder as Wheat-BRIDGES
 cd ..
