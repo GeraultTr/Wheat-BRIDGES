371f63852378dd267a54b0607351631f1e98b679
GeraultTr <tristan.gerault@inrae.fr>
2025-05-30 10:54:47 +0200

    reported changes manually after coding sprint 1

--- test/test_scene.py
+++ test/test_scene.py
@@ -7,16 +7,13 @@ from wheat_bridges.wheat_bridges import WheatBRIDGES
 
 # Utility packages
 from initialize.initialize import MakeScenarios as ms
-from metafspm.scene_wrapper import SceneWrapper
+from metafspm.scene_wrapper import play_Orchestra
 #from log.logging import Logger
 #from analyze.analyze import analyze_data
 
 
 if __name__ == "__main__":
-    scenarios = ms.from_table(file_path="inputs/Scenarios_24-09-13.xlsx", which=["WB6"])
-    scene = SceneWrapper(scene_name="Test_scene", planting_pattern=SceneWrapper.planting_pattern, plant_models=[WheatBRIDGES], plant_scenarios=[scenarios["WB6"]], soil_model=RhizosphericSoil)
-
-    for _ in range(2):
-        scene.play()
-
-    scene.close()
+    scenarios = ms.from_table(file_path="inputs/Scenarios_24_11_10.xlsx", which=["RC_ref"])
+    play_Orchestra(scene_name="Test_scene", output_folder="outputs", plant_models=[WheatBRIDGES], plant_scenarios=[scenarios["RC_ref"]], 
+                           soil_model=RhizosphericSoil, soil_scenario=scenarios["RC_ref"], 
+                           n_iterations=2500)
