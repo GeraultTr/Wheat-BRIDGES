e5c18034df95c93c3d492e86db8a71af8412ba0f
GeraultTr <tristan.gerault@inrae.fr>
2025-10-04 20:07:37 +0200

    AA exudation corrected

--- simulations/scene/simulation_3d_soil.py
+++ simulations/scene/simulation_3d_soil.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     scenarios = ms.from_table(file_path="inputs/Scenarios_25-08-05.xlsx", which=["WB_ref"])
     # output_folder = "outputs"
     output_folder = "outputs/parametrization"
-    custom_suffix = "p4_heavy"
+    custom_suffix = "p3_heavy2"
     # densities = [50, 200, 400]
     # densities = [50, 400]
     densities = [250]
@@ -77,7 +77,7 @@ if __name__ == "__main__":
                 clean_exit = play_Orchestra(scene_name=full_scenario_name, output_folder=output_folder, plant_models=[WheatBRIDGES], plant_scenarios=[scenario], 
                                     soil_model=RhizoSoil, soil_scenario=scenario, light_model=LightModel,
                                     translator_path=wheat_bridges.__path__[0],
-                                    logger_class=Logger, log_settings=Logger.heavy_log, heavy_log_period=6,
+                                    logger_class=Logger, log_settings=Logger.light_log, heavy_log_period=6,
                                     scene_xrange=scene_xrange, scene_yrange=scene_yrange, sowing_density=target_density, row_spacing=row_spacing,
                                     time_step=3600, n_iterations=2500, record_performance=True)
 
